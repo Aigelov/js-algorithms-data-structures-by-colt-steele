87fca7a5a7e5acca23feec9cf76c1880a0300cae
Aigelov <aigelov@gmail.com>
2020-03-15 18:18:15 +0600

    Bubble sort

---
+++ 11-bubble-sort/bubble-sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+// console.log(bubbleSort([7, 20, 15, 10, 3]));
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])); // noSwaps
