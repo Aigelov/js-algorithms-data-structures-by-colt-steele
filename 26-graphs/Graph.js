cca89582a2bbcf866109926c79a47fc399d6ae0e
Galymzhan Aigelov <galymzhan@otgroup.kz>
2020-07-20 00:10:47 +0600

    Graph Traversal (DFS Recursive, DFS Iterative, BFS)

--- 26-graphs/Graph.js
+++ 26-graphs/Graph.js
@@ -3,6 +3,7 @@ class Graph {
     this.adjacencyList = {};
   }
 
+
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
@@ -51,20 +52,118 @@ class Graph {
       );
     }
   }
+
+  // Depth First Search Recursive
+  DFS_Recursive(start) {
+    const list = [];
+    const visited = {};
+    const graph = this.adjacencyList;
+
+    const DFS = (vertex) => {
+      if (!vertex) return null;
+      if (!graph[vertex]) return list;
+
+      list.push(vertex);
+      visited[vertex] = true;
+
+      graph[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return DFS(neighbor);
+        }
+      });
+    }
+
+    DFS(start);
+
+    return list;
+  }
+
+  // Depth First Search Iterative
+  DFS_Iterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    const graph = this.adjacencyList;
+
+    visited[start] = true;
+
+    while(stack.length) {
+      const vertex = stack.pop();
+      result.push(vertex);
+
+      graph[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          stack.push(neighbor);
+          visited[neighbor] = true;
+        }
+      });
+    }
+
+    return result;
+  }
+
+  // Breadth First Search
+  BFS(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    const graph = this.adjacencyList;
+
+    visited[start] = true;
+
+    while(queue.length) {
+      const vertex = queue.shift();
+      result.push(vertex);
+
+      graph[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      })
+    }
+
+    return result;
+  }
 }
 
 const graph = new Graph();
-graph.addVertex('Tokyo');
-graph.addVertex('San Francisco');
-graph.addVertex('Seoul');
-graph.addVertex('Hong Kong');
-graph.addEdge('Tokyo', 'Seoul')
-graph.addEdge('Tokyo', 'San Francisco')
-graph.addEdge('Tokyo', 'Hong Kong')
-graph.addEdge('Seoul', 'San Francisco')
-graph.addEdge('Seoul', 'Hong Kong')
+// graph.addVertex('Tokyo');
+// graph.addVertex('San Francisco');
+// graph.addVertex('Seoul');
+// graph.addVertex('Hong Kong');
+// graph.addEdge('Tokyo', 'Seoul')
+// graph.addEdge('Tokyo', 'San Francisco')
+// graph.addEdge('Tokyo', 'Hong Kong')
+// graph.addEdge('Seoul', 'San Francisco')
+// graph.addEdge('Seoul', 'Hong Kong')
 // graph.removeEdge('Tokyo', 'Seoul');
 // graph.removeEdge2('San Francisco', 'Tokyo');
-graph.removeVertex('Tokyo');
+// graph.removeVertex('Tokyo');
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
 
 console.log(graph.adjacencyList);
+// console.log(graph.DFS_Recursive('A'));
+// console.log(graph.DFS_Iterative('A'));
+console.log(graph.BFS('A'));
